f9f01eed3764694fc6914268790167903149b1b2
hirakida <dantuba19990326@gmail.com>
2024-12-11 23:39:18 +0900

    Add: /likes/[:id]のパスとアクションの追加

--- live-chat-rails/app/controllers/likes_controller.rb
+++ live-chat-rails/app/controllers/likes_controller.rb
@@ -1,5 +1,5 @@
 class LikesController < ApplicationController
-  before_action :authenticate_user!, only: ['create']
+  before_action :authenticate_user!
 
   def create
     like = Like.new(message_id: params[:id], user_id: current_user.id)
@@ -8,6 +8,16 @@ class LikesController < ApplicationController
       render json: { id: like.id, email: current_user.email, message: '成功しました' }, status: 200
     else
       render json: { message: '保存出来ませんでした', errors: like.errors.messages }, status: 400
+    end
+  end
+
+  def destroy
+    like = Like.find(params[:id])
+
+    if like.destroy
+      render json: { id: like.id, email: like.user.email, message: '削除に成功しました' }, status: 200
+    else
+      render json: { message: '削除できませんでした', errors: like.errors.messages }, status: 400
     end
   end
 end
