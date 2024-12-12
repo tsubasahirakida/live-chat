3ee15f37483a74377596d02b8b48d07139feca0a
hirakida <dantuba19990326@gmail.com>
2024-12-12 21:04:22 +0900

    Add: 会員登録失敗時のバリデーションメッセージを追加

--- live-chat-vuejs/src/components/SignupForm.vue
+++ live-chat-vuejs/src/components/SignupForm.vue
@@ -6,6 +6,7 @@
       <input type="email" required placeholder="メールアドレス" v-model="email">
       <input type="password" required placeholder="パスワード" v-model="password">
       <input type="password" required placeholder="パスワード（確認用）" v-model="passwordConfirmation">
+      <div class="error">{{ error }}</div>
       <button>登録する</button>
     </form>
   </div>
@@ -20,11 +21,13 @@ export default {
       name: '',
       email: '',
       password: '',
-      passwordConfirmation: ''
+      passwordConfirmation: '',
+      error: null
     }
   },
   methods: {
     async signUp () {
+      this.error = null
       try {
         const res = await axios.post('http://localhost:3000/auth', {
           name: this.name,
@@ -33,10 +36,13 @@ export default {
           password_confirmation: this.passwordConfirmation
           }
         )
+        if (!res) {
+          throw new Error('アカウントを登録できませんでした')
+        }
         console.log({ res })
         return res
       } catch (error) {
-        console.log({ error })
+        this.error = 'アカウントを登録できませんでした'
       }
     }
   }
