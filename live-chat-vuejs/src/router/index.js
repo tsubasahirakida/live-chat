134e65689759d5154fb8d452817533ddf41e7065
hirakida <dantuba19990326@gmail.com>
2024-12-12 20:18:46 +0900

    Add: デモのWelcomePageの作成

--- live-chat-vuejs/src/router/index.js
+++ live-chat-vuejs/src/router/index.js
@@ -1,6 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import WelcomePage from '../views/WelcomePage'
 
-const routes = []
+const routes = [
+  {
+    path: '/',
+    name: 'WelcomePage',
+    component: WelcomePage
+  }
+]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
