f76d86e2953909d817c51e7d9eb07e1a87dd396e
hirakida <dantuba19990326@gmail.com>
2024-12-12 20:31:46 +0900

    Add: Test用のComponentの作成とpropsとオーバーライドの練習

--- live-chat-vuejs/src/views/WelcomePage.vue
+++ live-chat-vuejs/src/views/WelcomePage.vue
@@ -1,14 +1,18 @@
 <template>
   <div>
     <h1>{{ title }}</h1>
-    <p v-if="isEnabled">{{ subtitle }}</p>
-    <button @dblclick="toggle">トグルする</button>
-    <p>{{ text }}</p>
+    <TestComponent @toggle="toggle" message="コンポーネントにデータが渡されています" />
+    <p v-if="isEnabled">こんにちは</p>
   </div>
 </template>
 
 <script>
+import TestComponent from '../components/TestComponent.vue'
+
 export default {
+  components: {
+    TestComponent
+  },
   data () {
     return {
       title: '初めてのVue.jsアプリです！',
@@ -32,3 +36,10 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+  p {
+    font-size: 20px;
+    color: red;
+  }
+</style>
