1848411d68597dff5ea0d2a3cab5c81fb7f281a7
hirakida <dantuba19990326@gmail.com>
2024-12-13 22:04:29 +0900

    Add: ログアウト失敗時のエラーメッセージの表示

--- live-chat-vuejs/src/components/NavBar.vue
+++ live-chat-vuejs/src/components/NavBar.vue
@@ -3,6 +3,7 @@
     <div>
       <p>こんにちは、<span class="name">{{ name }}</span>さん</p>
       <p class="email">現在、 {{ email }} でログイン中です</p>
+      <div class="error">{{ error }}</div>
     </div>
     <button @click="logout">ログアウト</button>
   </nav>
@@ -15,11 +16,14 @@ export default {
   data () {
     return {
       name: window.localStorage.getItem('name'),
-      email: window.localStorage.getItem('uid')
+      email: window.localStorage.getItem('uid'),
+      error: null
     }
   },
   methods: {
     async logout () {
+      this.error = null
+
       try {
         const res = await axios.delete('http://localhost:3000/auth/sign_out', {
           headers: {
@@ -29,13 +33,19 @@ export default {
           }
         })
 
-        console.log("ログアウトしました")
-        window.localStorage.removeItem('access-token')
-        window.localStorage.removeItem('client')
-        window.localStorage.removeItem('uid')
-        window.localStorage.removeItem('name')
+        if (!res) {
+          new Error('ログアウトできませんでした')
+        }
 
-        this.$router.push({ name: 'Welcome' })
+        if (!this.error) {
+          console.log("ログアウトしました")
+          window.localStorage.removeItem('access-token')
+          window.localStorage.removeItem('client')
+          window.localStorage.removeItem('uid')
+          window.localStorage.removeItem('name')
+
+          this.$router.push({ name: 'Welcome' })
+        }
 
         return res
       } catch (error) {
