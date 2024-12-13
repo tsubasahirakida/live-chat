f79d776601d8dd3dcb19281024e229e30556f4ee
hirakida <dantuba19990326@gmail.com>
2024-12-13 22:27:30 +0900

    Add: 認証時は/chatroomに遷移できないように機能追加

--- live-chat-vuejs/src/router/index.js
+++ live-chat-vuejs/src/router/index.js
@@ -1,6 +1,30 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import WelcomePage from '../views/WelcomePage'
 import ChatRoom from '../views/ChatRoom'
+import useValidate from '../auth/validate'
+
+const { error, validate } = useValidate()
+
+const requireAuth = async (to, from, next) => {
+  const uid = window.localStorage.getItem('uid')
+  const client = window.localStorage.getItem('client')
+  const accessToken = window.localStorage.getItem('access-token')
+
+  if (!uid || !client || !accessToken) {
+    console.log('ログインしていません')
+    next({ name: 'WelcomePage' })
+    return
+  }
+
+  await validate()
+
+  if (error.value) {
+    console.log('認証に失敗しました')
+    next({ name: 'WelcomePage' })
+  } else {
+    next()
+  }
+}
 
 const routes = [
   {
@@ -11,7 +35,8 @@ const routes = [
   {
     path: '/chatroom',
     name: 'ChatRoom',
-    component: ChatRoom
+    component: ChatRoom,
+    beforeEnter: requireAuth
   }
 ]
 
