86965a273b294f5ba530e3e3d25524b46bb88153
hirakida <dantuba19990326@gmail.com>
2024-12-15 23:59:02 +0900

    Add: /chatroom遷移時にメッセージを全て取得

--- live-chat-vuejs/src/views/ChatRoom.vue
+++ live-chat-vuejs/src/views/ChatRoom.vue
@@ -1,14 +1,44 @@
 <template>
   <div class="container">
     <NavBar />
+    <ChatWindow :messages="messages" />
   </div>
 </template>
 
 <script>
 import NavBar from '../components/NavBar'
+import ChatWindow from '../components/ChatWindow'
+import axios from 'axios'
 
 export default {
-  components: { NavBar },
+  components: { NavBar, ChatWindow },
+  data () {
+    return {
+      messages: [],
+    }
+  },
+  methods: {
+    async getMessages () {
+      try {
+        const res = await axios.get('http://localhost:3000/messages', {
+          headers: {
+            uid: window.localStorage.getItem('uid'),
+            "access-token": window.localStorage.getItem('access-token'),
+            client:window.localStorage.getItem('client')
+          }
+        })
+        if (!res) {
+          new Error('メッセージ一覧を取得できませんでした')
+        }
+        this.messages = res.data
+      } catch (err) {
+        console.log(err)
+      }
+    },
+  },
+  mounted() {
+    this.getMessages()
+  },
 }
 </script>
 
