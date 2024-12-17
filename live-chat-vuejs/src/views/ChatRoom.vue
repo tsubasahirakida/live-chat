cb8b197750a476eb71f0ae2d88d4db42ac7b282a
hirakida <dantuba19990326@gmail.com>
2024-12-17 13:45:20 +0900

    mod: メッセージの日時を◯前の表示に修正

--- live-chat-vuejs/src/views/ChatRoom.vue
+++ live-chat-vuejs/src/views/ChatRoom.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container">
     <NavBar />
-    <ChatWindow @connectCable="connectCable" :messages="messages" />
+    <ChatWindow @connectCable="connectCable" :messages="formattedMessages" />
     <NewChatForm @connectCable="connectCable" />
   </div>
 </template>
@@ -12,12 +12,23 @@ import ChatWindow from '../components/ChatWindow'
 import NewChatForm from '../components/NewChatForm'
 import axios from 'axios'
 import ActionCable from 'actioncable'
+import { formatDistanceToNow } from 'date-fns'
+import { ja } from 'date-fns/locale'
 
 export default {
   components: { NavBar, ChatWindow, NewChatForm },
   data () {
     return {
       messages: [],
+    }
+  },
+  computed: {
+    formattedMessages () {
+      if (!this.messages.length) { return [] }
+      return this.messages.map(message => {
+        let time = formatDistanceToNow(new Date(message.created_at), { locale: ja })
+        return { ...message, created_at: time }
+      })
     }
   },
   methods: {
