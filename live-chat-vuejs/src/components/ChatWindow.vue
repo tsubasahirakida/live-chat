869b8837314036ec31f66f64eec0fa6f43a37c5e
hirakida <dantuba19990326@gmail.com>
2024-12-17 13:10:56 +0900

    Add: いいね数がfontawesomeのアイコンで表示されるように機能追加

--- live-chat-vuejs/src/components/ChatWindow.vue
+++ live-chat-vuejs/src/components/ChatWindow.vue
@@ -6,7 +6,10 @@
           <span class="name">{{ message.name }}</span>
           <div class="message" @dblclick="createLike(message.id)">
             {{ message.content }}
-            {{ message.likes.length }}
+            <div v-if="message.likes.length" class="heart-container">
+              <font-awesome-icon icon="heart" class="heart" />
+              <span class="heart-count">{{ message.likes.length }}</span>
+            </div>
           </div>
           <span class="created-at">{{ message.created_at }}</span>
         </li>
@@ -49,6 +52,7 @@ export default {
 }
 </script>
 
+<!-- ...省略 -->
 <style scoped>
   .chat-window {
     background: white;
@@ -99,9 +103,36 @@ export default {
     font-size: 12px;
     margin-bottom: 20px;
     margin-left: 4px;
+    margin-top: 3px;
   }
   .messages {
     max-height: 400px;
     overflow: auto;
+  }
+  .message {
+    position: relative;
+  }
+  .heart-container {
+    background: white;
+    position: absolute;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    border-radius: 30px;
+    min-width: 25px;
+    border-style: solid;
+    border-width: 1px;
+    border-color: rgb(245, 245, 245);
+    padding: 1px 2px;
+    z-index: 2;
+    bottom: -7px;
+    right: 0px;
+    font-size: 9px;
+  }
+  .heart {
+    color: rgb(236, 29, 29);
+  }
+  .heart-count {
+    color: rgb(20, 19, 19);
   }
 </style>
